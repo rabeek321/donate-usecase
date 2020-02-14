23cf2682e82f46c8db557f1782378f907ad45f63
Jeyakumar Sengottaiyan <rsjeyakumar@gmail.com>
2020-02-14 13:32:42 +0530

    Added dashboard contributors list

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -8,38 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   data: any;
 
-    constructor() {
-     
-    }
+  constructor() {
+
+  }
 
   ngOnInit() {
     this.data = {
       datasets: [{
-          data: [
-              11,
-              16,
-              7,
-              3,
-              14
-          ],
-          backgroundColor: [
-              '#FF6384',
-              '#4BC0C0',
-              '#FFCE56',
-              '#E7E9ED',
-              '#36A2EB'
-          ],
-          label: 'My dataset'
+        data: [
+          11,
+          16,
+          7,
+          3,
+          14
+        ],
+        backgroundColor: [
+          '#FF6384',
+          '#4BC0C0',
+          '#FFCE56',
+          '#E7E9ED',
+          '#36A2EB'
+        ],
+        label: 'My dataset'
       }],
       labels: [
-          'Red',
-          'Green',
-          'Yellow',
-          'Grey',
-          'Blue'
+        'Red',
+        'Green',
+        'Yellow',
+        'Grey',
+        'Blue'
       ]
-  };
+    };
 
+  }
+
+  getContributors($event) {
+    console.log($event);
   }
 
 }
