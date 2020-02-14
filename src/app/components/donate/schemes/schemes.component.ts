84a345d10d297f0634b897e0601397c4adf47be7
User1 <User1@LP-5CD9296CM7.HCLT.CORP.HCL.IN>
2020-02-14 15:21:28 +0530

    Latest Changes

--- src/app/components/donate/schemes/schemes.component.ts
+++ src/app/components/donate/schemes/schemes.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpService } from '../../../services/http.service';
+import { environment} from 'src/environments/environment';
+import { EndPoints } from '../../../services/endpoints.enum';
+import { SchemeRes, SchemeDetails } from '../../../models/models';
+import Swal from 'sweetalert2';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-schemes',
@@ -6,10 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schemes.component.css']
 })
 export class SchemesComponent implements OnInit {
+  loader = false;
+  categoryId: number;
+  schemesList: SchemeDetails[];
+  constructor(
+    private router: Router,
+    private routeNavigate: ActivatedRoute,
+    public donateService: HttpService) { }
 
-  constructor() { }
-
+    getCharityList(catId) {
+      this.loader = true;
+      const endpoints = `${environment.apiUrl}/${EndPoints.Categories}/${catId}/schemes`;
+      this.donateService.readData(endpoints).subscribe(res => {
+        console.log(res);
+        this.loader = false;
+        this.schemesList = res.schemeDetails;
+      },
+        error => {
+          this.loader = false;
+        });
+    }
   ngOnInit() {
+    this.routeNavigate.queryParams.subscribe(params => {
+      this.categoryId = params.categoryId;
+    });
+    this.getCharityList(this.categoryId);
   }
 
 }
