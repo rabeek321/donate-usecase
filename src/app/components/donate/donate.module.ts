d70ec3b3941e73c155f65df8fc365d28e00ebdf6
User1 <User1@LP-5CD9296CM7.HCLT.CORP.HCL.IN>
2020-02-14 13:26:06 +0530

    Donate module changes

--- src/app/components/donate/donate.module.ts
+++ src/app/components/donate/donate.module.ts
@@ -5,13 +5,16 @@ import { DonateRoutingModule } from './donate-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SchemesComponent } from './schemes/schemes.component';
 import { PaymentComponent } from './payment/payment.component';
-
+import { SharedModule } from '../../shared/shared.module';
+import { PrimengModule } from '../../shared/primeng/primeng.module';
 
 @NgModule({
   declarations: [HomeComponent, SchemesComponent, PaymentComponent],
   imports: [
     CommonModule,
-    DonateRoutingModule
+    DonateRoutingModule,
+    SharedModule,
+    PrimengModule
   ]
 })
 export class DonateModule { }
--- src/app/components/donate/home/home.component.ts
+++ src/app/components/donate/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpService } from '../../../services/http.service';
+import { environment} from 'src/environments/environment';
+import { EndPoints } from '../../../services/endpoints.enum';
+import Swal from 'sweetalert2';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+loader = false;
+charityList;
+  constructor(
+    private router: Router,
+    public donateService: HttpService) { }
 
-  constructor() { }
+    getCharityList() {
+      this.loader = true;
+      const endpoints = `${environment.apiUrl}/${EndPoints.userDetails}` ;
+      this.donateService.readData(endpoints).subscribe(res => {
+        console.log(res);
+        this.loader = false;
+        this.charityList = res.categoryDetails;
+      },
+        error => {
+          this.loader = false;
+        });
+    }
+
+    goToSchemes(val) {
+      this.router.navigate(['/donate/schemes'], { queryParams: { categoryId: val } });
+    }
 
   ngOnInit() {
   }
